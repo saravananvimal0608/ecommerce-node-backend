66751354f923f1c0bbeead16ed8e70ae79dfa730
saravananvimal21 <saravanan211118email@gmail.com>
2026-08-20 14:25:06 +0530

    updated email send method

--- utils/emailSend.js
+++ utils/emailSend.js
@@ -7,12 +7,12 @@ if (!process.env.BREVO_API_KEY) {
 const brevo = new BrevoClient({ apiKey: process.env.BREVO_API_KEY });
 
 export const emailSend = async ({ email, subject, html }) => {
-  const { body } = await brevo.transactionalEmails.sendTransacEmail({
+  const { data } = await brevo.transactionalEmails.sendTransacEmail({
     sender: { name: "Blinkit", email: "saravananvimal0608@gmail.com" },
     to: [{ email }],
     subject,
     htmlContent: html,
   });
 
-  console.log("Email sent: " + body.messageId);
+  console.log("Email sent: " + data.messageId);
 };
